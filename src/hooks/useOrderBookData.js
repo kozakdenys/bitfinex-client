e6567a6273955cfadba52a7da373e198d34540c3
Denys Stepico <denys_stepico@roms-MacBook-Pro.local>
2021-08-17 10:09:10 +0300

    Rebuild using rxjs

--- src/hooks/useOrderBookData.js
+++ src/hooks/useOrderBookData.js
@@ -1,48 +1,82 @@
-import { useEffect, useReducer, useState } from 'react';
-import reducer, { initialState } from '../store/orderBookReducer';
+import { useEffect, useState } from 'react';
+import { webSocket } from "rxjs/webSocket";
+import { bufferTime } from 'rxjs/operators';
+import getDataByKey from "../helpers/getDataByKey";
+import dataKeys from "../constants/dataKeys";
+
+const insertBookData = ({ bids, asks, payload }) => {
+  let newBids = bids;
+  let newAsks = asks;
+  let arrayToUpdate;
+
+  //is it bids or asks
+  if (getDataByKey(payload, dataKeys.amount) > 0) {
+    newBids = [...newBids];
+    arrayToUpdate = newBids;
+  } else {
+    newAsks = [...newAsks];
+    arrayToUpdate = newAsks;
+  }
+
+  const index = arrayToUpdate.findIndex(([price]) => price === getDataByKey(payload, dataKeys.price));
+
+  if (index === -1) {
+    //if the new item - add to the end and sort
+    arrayToUpdate.push(payload);
+    arrayToUpdate.sort((a, b) => getDataByKey(b, dataKeys.price) - getDataByKey(a, dataKeys.price))
+  } else if (getDataByKey(payload, dataKeys.count) === 0) {
+    //if count 0 - remove item from the array
+    arrayToUpdate.splice(index, 1);
+  } else {
+    //if item already exists - update it
+    arrayToUpdate[index] = payload;
+  }
+
+  return {
+    bids: newBids,
+    asks: newAsks,
+  }
+}
 
 export default function useOrderBookData(url, pair) {
-  const [lastUpdateDate, setLastUpdateDate] = useState(new Date());
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, setState] = useState({ bids: [], asks: [] });
 
   //create websocket
   useEffect(() => {
-    let chanelId;
-    const ws = new WebSocket(url);
-    const outMsg = {
+    const subject = webSocket(url);
+
+    subject
+        .pipe(bufferTime(500))
+        .subscribe(messages => {
+          if(!messages.length) return
+          setState(state => messages.reduce((state, inMsg) => {
+            if (Array.isArray(inMsg) && Array.isArray(inMsg[1])) {
+              const payload = inMsg[1]
+
+              if (Array.isArray(payload[0])) {
+                 return {
+                   bids: payload.slice(0, payload.length / 2),
+                   asks: payload.slice(-payload.length / 2)
+                 }
+              } else {
+                return insertBookData({
+                  ...state,
+                  payload
+                })
+              }
+            }
+            return state
+          }, state))
+        });
+
+    subject.next({
       event: 'subscribe',
       channel: 'book',
       pair
-    };
-    ws.onopen = () => ws.send(JSON.stringify(outMsg));
-    ws.onmessage = e => {
-      const inMsg = JSON.parse(e.data);
+    });
 
-      if (!inMsg) return;
-
-      if (!chanelId) {
-        //info message
-        if (typeof inMsg === 'object' && inMsg.event === 'subscribed' && inMsg.channel === outMsg.channel) {
-          chanelId = inMsg.chanId
-        }
-      } else {
-        //data message
-        if (Array.isArray(inMsg) && inMsg[0] === chanelId && Array.isArray(inMsg[1])) {
-          dispatch({ type: 'ADD_HOLDING_DATA', payload: inMsg[1] });
-        }
-      }
-    };
-
-    return () => ws.close();
-  }, [dispatch, url, pair]);
-
-  //trigger state update every 500 milliseconds
-  useEffect(() => {
-    setTimeout(() => {
-      setLastUpdateDate(new Date());
-      dispatch({ type: 'SAVE_HOLDING_DATA' });
-    }, 500)
-  }, [dispatch, lastUpdateDate]);
+    return () => subject.complete();
+  }, [url, pair]);
 
   return [state.bids, state.asks];
 };
